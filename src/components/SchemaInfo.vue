8f37d22faf68bf78d6f0fd638517bbb63f5dd89a
Tyler Mercer <tyleralanmercer@gmail.com>
2020-08-23 00:37:20 -0600

    Move CSV schema info to JSON file

--- src/components/SchemaInfo.vue
+++ src/components/SchemaInfo.vue
@@ -9,69 +9,27 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
+      <tr v-for="[field, description] in Object.entries(schema)" :key="field">
         <td>
-          <code>title</code>
+          <code>{{field}}</code>
         </td>
-        <td>
-          The title of a given card.{{" "}}
-          Shown in bold at the top of the card's front content.
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <code>body</code>
-        </td>
-        <td>
-          The body text of a card.{{" "}}
-          Shown in the card's front content, below the title.{{" "}}
-          (This field supports Markdown!)
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <code>backText</code>
-        </td>
-        <td>
-          The text to display on the back of the card.{{" "}}
-          Rendered in large lettering{{" "}}
-          (so be judicious about how much text you put here).{{" "}}
-          Useful for identifying which deck a card belongs to{{" "}}
-          (for games that have multiple decks).
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <code>frontColor</code>
-        </td>
-        <td>
-          The color of the front of the card.{{" "}}
-          Must be a valid CSS color (e.g.{{" "}}
-          <code>#FFA500</code>{{" "}}
-          or{{" "}}
-          <code>lightblue</code>).
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <code>backColor</code>
-        </td>
-        <td>
-          The color of the back of the card.{{" "}}
-          Must be a valid CSS color.
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <code>quantity</code>
-        </td>
-        <td>
-          How many copies of this card should be included in the deck.{{" "}}
-          Must be a number literal, e.g.{{" "}}
-          <code>10</code>
-        </td>
+        <td v-html="md.render(description)"></td>
       </tr>
     </tbody>
   </table>
   </details>
 </template>
+
+<script>
+import schema from '@/assets/csv-schema.json';
+import MarkdownIt from 'markdown-it';
+
+export default {
+  data() {
+    return {
+      schema,
+      md: new MarkdownIt()
+    }
+  }
+}
+</script>
